ecbdadea7b0ad1970ae52fc256c60590b2772d5e
Lucas Zhang <lhzhang@live.unc.edu>
2021-07-31 21:13:51 -0400

    Modified target endpoint to avoid overfetching

--- strapi-backend/api/target/controllers/target.js
+++ strapi-backend/api/target/controllers/target.js
@@ -1,8 +1,24 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.target.search(ctx.query);
+    } else {
+      entities = await strapi.services.target.find(ctx.query);
+    }
+
+    return entities.map(entity => {
+      const sanitized = sanitizeEntity(entity, { model: strapi.models.target })
+      if (sanitized["Sections"] && !ctx.query.UID) delete sanitized["Sections"];
+      return sanitized;
+    });
+  }
+};
