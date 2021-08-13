653b78fa9d9d74ce7608f3d0b8e7e8cbb160fe0e
Lucas Zhang <lhzhang@live.unc.edu>
2021-08-13 18:25:41 -0400

    Fixing up the layout of the hexagons

--- cdc-frontend-app/src/components/Collage/index.jsx
+++ cdc-frontend-app/src/components/Collage/index.jsx
@@ -9,9 +9,9 @@ import {
 import theme from "../../utilities/theme.module.scss";
 import "./styles.scss";
 
-function CollageItem({ children, color, position, size = 10 }) {
+function CollageItem({ children, color, position, size = '20%' }) {
   return (
-    <div className="CollageItem" style={{ ...position, width: `${size}rem` }}>
+    <div className="CollageItem" style={{ ...position, width: size }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 304 352"
@@ -27,30 +27,30 @@ function CollageItem({ children, color, position, size = 10 }) {
 function Collage() {
   return (
     <div className="Collage">
-      <CollageItem color="secondary" position={{ top: 360, left: 300 }}>
+      <CollageItem color="secondary" position={{ top: '50%', left: '30%' }} size="28%">
         <BeakerIcon />
       </CollageItem>
       <CollageItem
         color="accent1"
-        position={{ top: 140, right: 200 }}
-        size={16}
+        position={{ top: '20%', right: '20%' }}
+        size='40%'
       >
         <CalculatorIcon />
       </CollageItem>
       <CollageItem
         color="accent2"
-        position={{ bottom: 180, right: 160 }}
-        size={12}
+        position={{ bottom: '25%', right: '22%' }}
+        size='22%'
       >
         <ChartPieIcon />
       </CollageItem>
-      <CollageItem color="accent3" position={{ bottom: 300, left: 200 }}>
+      <CollageItem color="accent3" position={{ bottom: '48%', left: '24%' }} size="22%">
         <BookOpenIcon />
       </CollageItem>
       <CollageItem
         color="accent4"
-        position={{ bottom: 140, left: 200 }}
-        size={8}
+        position={{ bottom: '30%', left: '10%' }}
+        size='25%'
       >
         <CodeIcon />
       </CollageItem>
