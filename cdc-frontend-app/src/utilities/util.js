b573428ee23dae13e02ff3f2de37c350ba6a7e09
Lucas Zhang <lhzhang@live.unc.edu>
2021-08-15 17:37:42 -0400

    http to https

--- cdc-frontend-app/src/utilities/util.js
+++ cdc-frontend-app/src/utilities/util.js
@@ -1,5 +1,5 @@
 export function handleAPIURL() {
-  return `http://${process.env.NODE_ENV === "development"
+  return `https://${process.env.NODE_ENV === "development"
     ? "localhost:1337"
     : "api.cdcunc.com"
     }`;
