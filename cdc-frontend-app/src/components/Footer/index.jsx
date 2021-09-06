41cee2c03dbc35f54345877239e7d8f0bd1c3449
Lucas Zhang <lhzhang@live.unc.edu>
2021-09-06 12:06:37 -0400

    Swapped facebook and linkedin buttons

--- cdc-frontend-app/src/components/Footer/index.jsx
+++ cdc-frontend-app/src/components/Footer/index.jsx
@@ -22,19 +22,6 @@ function Footer() {
         </ButtonLink>
         <ButtonLink
           className="Footer-links"
-          id="linkedin-button"
-          to="https://www.linkedin.com/company/carolinadatachallenge/"
-        >
-          <svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            className="button-icon"
-          >
-            <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286h-.001zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
-          </svg>
-        </ButtonLink>
-        <ButtonLink
-          className="Footer-links"
           id="facebook-button"
           to="https://www.facebook.com/events/381296296903322"
         >
@@ -44,6 +31,19 @@ function Footer() {
             className="button-icon"
           >
             <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.67 4.533-4.67 1.312 0 2.686.236 2.686.236v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"></path>
+          </svg>
+        </ButtonLink>
+        <ButtonLink
+          className="Footer-links"
+          id="linkedin-button"
+          to="https://www.linkedin.com/company/carolinadatachallenge/"
+        >
+          <svg
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            className="button-icon"
+          >
+            <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286h-.001zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
           </svg>
         </ButtonLink>
         <ButtonLink className="Footer-links" id="mail-button" to="mailto:hello@cdcunc.com">
