7540c01769cd0d45a4a078ca7c7d29cac4cfd061
Lucas Zhang <lhzhang@live.unc.edu>
2021-09-13 22:17:29 -0400

    Added cads button to footer

--- cdc-frontend-app/src/components/Footer/index.jsx
+++ cdc-frontend-app/src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { ButtonLink } from "../Button";
+import CADS from "../../assets/cads-logo-21.svg";
 import "./styles.scss";
 
 function Footer() {
@@ -46,7 +47,11 @@ function Footer() {
             <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286h-.001zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"></path>
           </svg>
         </ButtonLink>
-        <ButtonLink className="Footer-links" id="mail-button" to="mailto:hello@cdcunc.com">
+        <ButtonLink
+          className="Footer-links"
+          id="mail-button"
+          to="mailto:hello@cdcunc.com"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="button-icon"
@@ -56,10 +61,13 @@ function Footer() {
             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
           </svg>
         </ButtonLink>
-        {/* <ButtonLink
+        <ButtonLink
           className="Footer-links"
-          to="https://www.google.com"
-        ></ButtonLink> */}
+          id="cads-button"
+          to="https://carolinadata.unc.edu/"
+        >
+          <img src={CADS} />
+        </ButtonLink>
       </div>
       <div>
         <ReactMarkdown>
